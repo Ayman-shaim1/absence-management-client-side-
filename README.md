13c91b1b7102e135309224fe0e35cdb5dbf2d697
Ayma-shaim1 <ayman.shaim10@gmail.com>
2023-01-15 17:29:14 +0100

    Initial commit -m

---
+++ README.md
@@ -0,0 +1,9 @@
+# absence-managment [(client-side) - (desktop-app)]
+
+### 1. installer les dependences :
+
+npm install --save
+
+### 2. lancement de l'application :
+
+npm start
---
+++ main.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const { app, BrowserWindow } = require("electron");
+
+
+const isDev = process.env.NODE_ENV !== "production";
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    title: "Gestion d'absence",
+    autoHideMenuBar: true,
+    resizable: false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  if(isDev){
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
+  mainWindow.maximize();
+  mainWindow.show();
+};
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
