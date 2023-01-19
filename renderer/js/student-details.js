50cd593cae27310050c345d12bc2c6be9170e8a0
Ayma-shaim1 <ayman.shaim10@gmail.com>
2023-01-19 21:47:22 +0100

    Send sms

--- renderer/js/student-details.js
+++ renderer/js/student-details.js
@@ -1,5 +1,8 @@
 const studentInfoTable = document.getElementById("student-info-table");
 const studentImage = document.getElementById("student-image");
+const frmSendSms = document.getElementById("frm-send-sms");
+const txtPhone = document.getElementById("txt-phone");
+const txtTextContent = document.getElementById("txt-text-content");
 
 const getUserInfo = async () => {
   const urlSearchParams = new URLSearchParams(window.location.search);
@@ -8,6 +11,7 @@ const getUserInfo = async () => {
   const { data } = await axios.get(`${env.apirurl}/api/students/${id}`);
   showUserInfo(data);
   studentImage.setAttribute("src", data.image);
+  txtPhone.value = data.phone;
 };
 
 const showUserInfo = studentinfo => {
@@ -37,3 +41,37 @@ document.addEventListener("DOMContentLoaded", () => {
     getUserInfo();
   }
 });
+
+frmSendSms.addEventListener("submit", e => {
+  e.preventDefault();
+  const phone = txtPhone.value;
+  const textContent = txtTextContent.value;
+  sendSms(phone, textContent);
+});
+
+const sendSms = async (to, text) => {
+  try {
+    const { data } = await axios.post(env.apirurl + "/api/sms/" + to, { text });
+    Swal.fire({
+      title: "Validation",
+      text: "SMS sent successfully",
+      icon: "success",
+      confirmButtonText: "OK",
+      confirmButtonColor: "var(--bs-success)",
+    });
+    txtPhone.value = "";
+    txtTextContent.value = "";
+  } catch (error) {
+    const err =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    Swal.fire({
+      title: "Error !",
+      text: err,
+      icon: "error",
+      confirmButtonText: "OK",
+      confirmButtonColor: "var(--bs-danger)",
+    });
+  }
+};
